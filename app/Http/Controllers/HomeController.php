56b1b1bed794aad7a64129e6ab4fde35458961a9
KennDeClouv <146813701+KennDeClouv@users.noreply.github.com>
2024-12-19 16:10:31 +0700

    fix: Call to a member function where() on null in Homecontroller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace App\Http\Controllers;
+
 use Illuminate\Support\Facades\Auth;
 use App\Models\Imam;
 use App\Models\Masjid;
@@ -7,6 +9,8 @@ use App\Models\Schedule;
 use App\Models\Announcement;
 use App\Models\Quote;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Log;
+
 class HomeController extends Controller
 {
     public function adminHome()
@@ -19,29 +23,45 @@ class HomeController extends Controller
         $startOfMonth = Carbon::now()->startOfMonth();
         $endOfMonth = Carbon::now()->endOfMonth();
         $weeklyJadwal = Schedule::whereBetween('date', [$startOfWeek, $endOfWeek])->count();
-        $bayaranImam = Schedule::where('status', 'done')
-            ->whereBetween('date', [$startOfMonth, $endOfMonth])
-            ->with(['Imam.ListFee.Fee'])
-            ->get()
-            ->reduce(function ($carry, $schedule) {
-                $imamFee = $schedule->Imam->ListFee
-                    ->where('shalat_id', $schedule->shalat_id)
-                    ->first()->Fee->amount
-                    ?? $schedule->Imam->ListFee
-                    ->where('shalat_id', null)
-                    ->first()->Fee->amount
-                    ?? 0;
 
-                return $carry + $imamFee;
-            }, 0);
+        try {
+            $bayaranImam = Schedule::where('status', 'done')
+                ->whereBetween('date', [$startOfMonth, $endOfMonth])
+                ->with(['Imam.ListFee.Fee'])
+                ->get()
+                ->reduce(function ($carry, $schedule) {
+                    $imamListFee = optional($schedule->Imam)->ListFee;
+
+                    if (!$imamListFee) {
+                        return $carry;
+                    }
+
+                    $imamFee = $imamListFee->where('shalat_id', $schedule->shalat_id)
+                        ->first()
+                        ?->Fee->amount
+                        ?? $imamListFee->where('shalat_id', null)
+                        ->first()
+                        ?->Fee->amount
+                        ?? 0;
+
+                    return $carry + $imamFee;
+                }, 0);
+        } catch (\Exception $e) {
+            Log::error("Error calculating Imam's fee: " . $e->getMessage());
+            $bayaranImam = 0;
+        }
+
+
         $schedules = Schedule::where('status', 'to_do')
             ->where('is_badal', true)
             ->whereNull('badal_id')
             ->get();
         $announcements = Announcement::all();
         $quote = Quote::where('status', true)->first();
+
         return view("{$role}.index", compact('imams', 'masjids', 'weeklyJadwal', 'schedules', 'announcements', 'bayaranImam', 'quote'));
     }
+
     public function imamHome()
     {
         $schedules = Schedule::where('status', 'to_do')
@@ -53,9 +73,10 @@ class HomeController extends Controller
         $announcements = Announcement::where('is_active', 1)
             ->where('target_id', Auth::user()->role->id)
             ->whereMonth('date', now()->month)
-
             ->get();
+
         $quote = Quote::where('status', true)->first();
+
         return view('imam.index', compact('schedules', 'announcements', 'quote'));
     }
 }
