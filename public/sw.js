833a987092b56d243b971268f30ece68a4a667c5
KennDeClouv <146813701+KennDeClouv@users.noreply.github.com>
2025-02-28 23:26:44 +0700

    fix: Bypass service worker cache for login page

--- public/sw.js
+++ public/sw.js
@@ -28,9 +28,14 @@ self.addEventListener("install", (event) => {
 });
 
 self.addEventListener("fetch", (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
-    );
+    if (event.request.url.includes("/login")) {
+        // Bypass cache untuk halaman login
+        event.respondWith(fetch(event.request));
+    } else {
+        event.respondWith(
+            caches.match(event.request).then((response) => {
+                return response || fetch(event.request);
+            })
+        );
+    }
 });
